c7884adf55d8b5febf59f23e947e459c5cc6b000
blaszko <blazej.zg@gmail.com>
2017-12-26 18:41:30 +0100

    dodanie formy

---
+++ Zadania_z_cwiczen_sl/Kalkulator/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Kalkulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbWynik = new System.Windows.Forms.TextBox();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.bOdejmij = new System.Windows.Forms.Button();
+            this.bPomnoz = new System.Windows.Forms.Button();
+            this.bDodaj = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bWynik = new System.Windows.Forms.Button();
+            this.bPodziel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbWynik
+            // 
+            this.tbWynik.Location = new System.Drawing.Point(12, 12);
+            this.tbWynik.Name = "tbWynik";
+            this.tbWynik.Size = new System.Drawing.Size(166, 20);
+            this.tbWynik.TabIndex = 0;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(12, 53);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(37, 41);
+            this.b7.TabIndex = 1;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(98, 53);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(37, 41);
+            this.b9.TabIndex = 2;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(55, 53);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(37, 41);
+            this.b8.TabIndex = 3;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(55, 147);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(37, 41);
+            this.b2.TabIndex = 6;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(98, 147);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(37, 41);
+            this.b3.TabIndex = 5;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(12, 147);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(37, 41);
+            this.b1.TabIndex = 4;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(55, 100);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(37, 41);
+            this.b5.TabIndex = 9;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(98, 100);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(37, 41);
+            this.b6.TabIndex = 8;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(12, 100);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(37, 41);
+            this.b4.TabIndex = 7;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            // 
+            // bOdejmij
+            // 
+            this.bOdejmij.Location = new System.Drawing.Point(141, 100);
+            this.bOdejmij.Name = "bOdejmij";
+            this.bOdejmij.Size = new System.Drawing.Size(37, 41);
+            this.bOdejmij.TabIndex = 12;
+            this.bOdejmij.Text = "-";
+            this.bOdejmij.UseVisualStyleBackColor = true;
+            // 
+            // bPomnoz
+            // 
+            this.bPomnoz.Location = new System.Drawing.Point(141, 147);
+            this.bPomnoz.Name = "bPomnoz";
+            this.bPomnoz.Size = new System.Drawing.Size(37, 41);
+            this.bPomnoz.TabIndex = 11;
+            this.bPomnoz.Text = "*";
+            this.bPomnoz.UseVisualStyleBackColor = true;
+            // 
+            // bDodaj
+            // 
+            this.bDodaj.Location = new System.Drawing.Point(141, 53);
+            this.bDodaj.Name = "bDodaj";
+            this.bDodaj.Size = new System.Drawing.Size(37, 41);
+            this.bDodaj.TabIndex = 10;
+            this.bDodaj.Text = "+";
+            this.bDodaj.UseVisualStyleBackColor = true;
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(12, 194);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(37, 41);
+            this.b0.TabIndex = 13;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            // 
+            // bWynik
+            // 
+            this.bWynik.Location = new System.Drawing.Point(55, 194);
+            this.bWynik.Name = "bWynik";
+            this.bWynik.Size = new System.Drawing.Size(80, 41);
+            this.bWynik.TabIndex = 14;
+            this.bWynik.Text = "=";
+            this.bWynik.UseVisualStyleBackColor = true;
+            // 
+            // bPodziel
+            // 
+            this.bPodziel.Location = new System.Drawing.Point(141, 194);
+            this.bPodziel.Name = "bPodziel";
+            this.bPodziel.Size = new System.Drawing.Size(37, 41);
+            this.bPodziel.TabIndex = 15;
+            this.bPodziel.Text = "/";
+            this.bPodziel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(199, 250);
+            this.Controls.Add(this.bPodziel);
+            this.Controls.Add(this.bWynik);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.bOdejmij);
+            this.Controls.Add(this.bPomnoz);
+            this.Controls.Add(this.bDodaj);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.tbWynik);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbWynik;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button bOdejmij;
+        private System.Windows.Forms.Button bPomnoz;
+        private System.Windows.Forms.Button bDodaj;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bWynik;
+        private System.Windows.Forms.Button bPodziel;
+    }
+}
+
