36c0d757d39c88820f527d87b0fee5faadb6bbb7
blaszko <blazej.zg@gmail.com>
2018-01-09 23:27:10 +0100

    sprwadzenie plci

---
+++ Zadania_z_cwiczen_sl/PESEL/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace PESEL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPesel = new System.Windows.Forms.TextBox();
+            this.labelPodajNrPesel = new System.Windows.Forms.Label();
+            this.labelDataUrodzenia = new System.Windows.Forms.Label();
+            this.labelPlec = new System.Windows.Forms.Label();
+            this.labelSprwadzPesel = new System.Windows.Forms.Label();
+            this.bSprawdzPesel = new System.Windows.Forms.Button();
+            this.bWyczyscFormularz = new System.Windows.Forms.Button();
+            this.tbDataUrodzenia = new System.Windows.Forms.TextBox();
+            this.tbPlec = new System.Windows.Forms.TextBox();
+            this.tbSprawdzPesel = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbPesel
+            // 
+            this.tbPesel.Location = new System.Drawing.Point(17, 24);
+            this.tbPesel.Name = "tbPesel";
+            this.tbPesel.Size = new System.Drawing.Size(155, 20);
+            this.tbPesel.TabIndex = 0;
+            // 
+            // labelPodajNrPesel
+            // 
+            this.labelPodajNrPesel.AutoSize = true;
+            this.labelPodajNrPesel.Location = new System.Drawing.Point(14, 8);
+            this.labelPodajNrPesel.Name = "labelPodajNrPesel";
+            this.labelPodajNrPesel.Size = new System.Drawing.Size(75, 13);
+            this.labelPodajNrPesel.TabIndex = 1;
+            this.labelPodajNrPesel.Text = "Podaj nr Pesel";
+            // 
+            // labelDataUrodzenia
+            // 
+            this.labelDataUrodzenia.AutoSize = true;
+            this.labelDataUrodzenia.Location = new System.Drawing.Point(14, 124);
+            this.labelDataUrodzenia.Name = "labelDataUrodzenia";
+            this.labelDataUrodzenia.Size = new System.Drawing.Size(79, 13);
+            this.labelDataUrodzenia.TabIndex = 2;
+            this.labelDataUrodzenia.Text = "Data urodzenia";
+            // 
+            // labelPlec
+            // 
+            this.labelPlec.AutoSize = true;
+            this.labelPlec.Location = new System.Drawing.Point(14, 176);
+            this.labelPlec.Name = "labelPlec";
+            this.labelPlec.Size = new System.Drawing.Size(28, 13);
+            this.labelPlec.TabIndex = 3;
+            this.labelPlec.Text = "Plec";
+            // 
+            // labelSprwadzPesel
+            // 
+            this.labelSprwadzPesel.AutoSize = true;
+            this.labelSprwadzPesel.Location = new System.Drawing.Point(14, 225);
+            this.labelSprwadzPesel.Name = "labelSprwadzPesel";
+            this.labelSprwadzPesel.Size = new System.Drawing.Size(120, 13);
+            this.labelSprwadzPesel.TabIndex = 4;
+            this.labelSprwadzPesel.Text = "Czy pesel jest poprawny";
+            // 
+            // bSprawdzPesel
+            // 
+            this.bSprawdzPesel.Location = new System.Drawing.Point(23, 76);
+            this.bSprawdzPesel.Name = "bSprawdzPesel";
+            this.bSprawdzPesel.Size = new System.Drawing.Size(111, 23);
+            this.bSprawdzPesel.TabIndex = 5;
+            this.bSprawdzPesel.Text = "Sprawdz Pesel";
+            this.bSprawdzPesel.UseVisualStyleBackColor = true;
+            this.bSprawdzPesel.Click += new System.EventHandler(this.bSprawdzPesel_Click);
+            // 
+            // bWyczyscFormularz
+            // 
+            this.bWyczyscFormularz.Location = new System.Drawing.Point(147, 76);
+            this.bWyczyscFormularz.Name = "bWyczyscFormularz";
+            this.bWyczyscFormularz.Size = new System.Drawing.Size(108, 23);
+            this.bWyczyscFormularz.TabIndex = 6;
+            this.bWyczyscFormularz.Text = "Wyczysc formularz";
+            this.bWyczyscFormularz.UseVisualStyleBackColor = true;
+            this.bWyczyscFormularz.Click += new System.EventHandler(this.bWyczyscFormularz_Click);
+            // 
+            // tbDataUrodzenia
+            // 
+            this.tbDataUrodzenia.Location = new System.Drawing.Point(17, 140);
+            this.tbDataUrodzenia.Name = "tbDataUrodzenia";
+            this.tbDataUrodzenia.Size = new System.Drawing.Size(155, 20);
+            this.tbDataUrodzenia.TabIndex = 7;
+            // 
+            // tbPlec
+            // 
+            this.tbPlec.Location = new System.Drawing.Point(17, 192);
+            this.tbPlec.Name = "tbPlec";
+            this.tbPlec.Size = new System.Drawing.Size(155, 20);
+            this.tbPlec.TabIndex = 8;
+            // 
+            // tbSprawdzPesel
+            // 
+            this.tbSprawdzPesel.Location = new System.Drawing.Point(17, 241);
+            this.tbSprawdzPesel.Name = "tbSprawdzPesel";
+            this.tbSprawdzPesel.Size = new System.Drawing.Size(155, 20);
+            this.tbSprawdzPesel.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 298);
+            this.Controls.Add(this.tbSprawdzPesel);
+            this.Controls.Add(this.tbPlec);
+            this.Controls.Add(this.tbDataUrodzenia);
+            this.Controls.Add(this.bWyczyscFormularz);
+            this.Controls.Add(this.bSprawdzPesel);
+            this.Controls.Add(this.labelSprwadzPesel);
+            this.Controls.Add(this.labelPlec);
+            this.Controls.Add(this.labelDataUrodzenia);
+            this.Controls.Add(this.labelPodajNrPesel);
+            this.Controls.Add(this.tbPesel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pesel Walidator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox tbPesel;
+        private System.Windows.Forms.Label labelPodajNrPesel;
+        private System.Windows.Forms.Label labelDataUrodzenia;
+        private System.Windows.Forms.Label labelPlec;
+        private System.Windows.Forms.Label labelSprwadzPesel;
+        private System.Windows.Forms.Button bSprawdzPesel;
+        private System.Windows.Forms.Button bWyczyscFormularz;
+        public System.Windows.Forms.TextBox tbDataUrodzenia;
+        public System.Windows.Forms.TextBox tbPlec;
+        public System.Windows.Forms.TextBox tbSprawdzPesel;
+    }
+}
+
---
+++ Zadania_z_cwiczen_sl/PESEL/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PESEL
+{
+
+    public partial class Form1 : Form
+    {
+        string pesel = "abcdef";
+
+        string plec;
+        string poprawnyPesel;
+        string dataUrodzenia;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bWyczyscFormularz_Click(object sender, EventArgs e)
+        {
+            tbDataUrodzenia.Text = "";
+            tbPesel.Text = "";
+            tbPlec.Text = "";
+            tbSprawdzPesel.Text = "";
+        }
+
+        private void bSprawdzPesel_Click(object sender, EventArgs e)
+        {
+            int dlugoscPesel;
+            bool czyNumer;
+
+
+            pesel = tbPesel.Text;
+
+            dlugoscPesel = pesel.Count();
+
+            if ((dlugoscPesel != 11) || pesel.Contains("+") || pesel.Contains("-"))
+            {
+                bWyczyscFormularz.PerformClick();
+            }
+
+            czyNumer = Regex.IsMatch(pesel, @"^\d+$");
+            if (!czyNumer)
+            {
+                bWyczyscFormularz.PerformClick();
+            }
+
+            int liczbaPlec;
+            liczbaPlec = int.Parse(pesel.Substring(6, 3));
+
+            if (liczbaPlec % 2 == 0)
+                tbPlec.Text = "Kobieta";
+            else
+                tbPlec.Text = "Mężczyzna";
+
+        }
+    }
+}
